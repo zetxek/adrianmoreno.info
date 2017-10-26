57f0ef1f86e3b93ee5e3b8d459253a4d398e8535
Adrián Moreno Peña <zetxek@gmail.com>
2017-10-26 14:57:40 +0200

    (Not used, comitting for reference)

--- mail.php
+++ mail.php
@@ -1,18 +1,42 @@
 <?php
 
-$Name = Trim(stripslashes($_POST['name'])); 
-$Email = Trim(stripslashes($_POST['email'])); 
-$Message = Trim(stripslashes($_POST['message'])); 
+$Name = Trim(stripslashes($_POST['name']));
+$Email = Trim(stripslashes($_POST['email']));
+$Message = Trim(stripslashes($_POST['message']));
 
 $EmailFrom = "contacto@adrianmoreno.info"; // 'From' Email
-$EmailTo = "zetxek@yahoo.es"; // Your Email (Where the Messages Are Sent)
-$Subject = "Mensaje a travŽes de AdrianMoreno.info"; // Email Subject Title
+$EmailTo = "zetxek@gmail.com"; // Your Email (Where the Messages Are Sent)
+$Subject = "Message from adrianmoreno.info"; // Email Subject Title
 
 // Validation
-$validationOK=true;
-if (!$validationOK) {
-  exit;
+$validationOK=false;
+
+$captcha = Trim(stripslashes($_POST['g-recaptcha-response']));
+
+
+########## ---- CAPTCHA
+$post_data = http_build_query(
+    array(
+        'secret' => '---getyourownsecret!----',
+        'response' => $_POST['g-recaptcha-response'],
+        'remoteip' => $_SERVER['REMOTE_ADDR']
+    )
+);
+$opts = array('http' =>
+    array(
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/x-www-form-urlencoded',
+        'content' => $post_data
+    )
+);
+$context  = stream_context_create($opts);
+$response = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
+$result = json_decode($response);
+if (!$result->success) {
+    die('Oh! CAPTCHA verification failed. Please email me directly at: zetxek at gmail dot com');
 }
+########## ---- CAPTCHA
+
 
 // Setup the Body of the Email
 $Body = "";
@@ -26,6 +50,8 @@ $Body .= "Mensaje: ";
 $Body .= $Message;
 $Body .= "\n";
 
-// Send Email 
+// Send Email
 $success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+header("Location: index.php?success=true");
+die();
 ?>
