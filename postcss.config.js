7f42ef751af30af2a85ea490cba093aa41070aa2
Adrián Moreno Peña <zetxek@gmail.com>
2022-12-30 09:51:22 +0100

    Purge CSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,15 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
     content: ["./hugo_stats.json"],
+    keyframes: true,
     defaultExtractor: (content) => {
       const els = JSON.parse(content).htmlElements;
       return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
     },
-    safelist: [],
+    variables: true,
+    safelist: {
+    greedy: [/header.*/, /.*icon.*/, /btn$/, /.*\[class.*/]
+    },
+    dynamicAttributes: ["type"]
   });
   
   module.exports = {
