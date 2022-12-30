c828fe56672bc04ccaa08f487138a26752e8160a
Adrián Moreno Peña <zetxek@gmail.com>
2022-12-30 10:16:09 +0100

    Update build script

--- vercel-build.sh
+++ vercel-build.sh
@@ -5,8 +5,8 @@ echo "Vercel URL: " $VERCEL_URL;
 
 if [ -z ${PROD_URL+x} ]; then
     echo "Not production deployment with VERCEL_URL: '$VERCEL_URL'";
-    hugo -b https://$VERCEL_URL -D --gc
+    hugo -b https://$VERCEL_URL -D --gc --minify --environment=production  
 else
     echo "Production URL with PROD_URL: '$PROD_URL'";
-    hugo -b https://$PROD_URL --gc
+    hugo -b https://$PROD_URL --gc --minify --environment=production
 fi
