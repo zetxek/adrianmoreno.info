2f0c72096fd45f0251dd2ce207c36a6d68535502
zetxek <zetxek@users.noreply.github.com>
2025-01-13 14:46:49 +0000

    deploy: 5c765ce3e81f0547ae1303f1591b008ee1a75847

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,1 +1,45 @@
-class StickyHeader{constructor(t){this.header=t,this.body=document.querySelector("body"),this.thresholdPosition=15,this.triggeredStickyClass="header--sticky-triggered",this.stickyClass="header--sticky",this.ticking=!1,this.bodyPosition=0,window.addEventListener("DOMContentLoaded",()=>this.initSticky()),this.scrollChanged()}initSticky(){this.headerStaticHeight=this.header.getBoundingClientRect().height,this.header.classList.toggle(this.stickyClass,!0),window.addEventListener("scroll",()=>this.scrollHandler())}scrollHandler(){this.ticking||(window.requestAnimationFrame(()=>{this.scrollChanged(),this.ticking=!1}),this.ticking=!0)}scrollChanged(){this.bodyPosition=Math.abs(this.body.getBoundingClientRect().top),this.bodyPosition>this.thresholdPosition?this.header.classList.toggle(this.triggeredStickyClass,!0):this.header.classList.toggle(this.triggeredStickyClass,!1)}}const stickyHeader=new StickyHeader(document.querySelector(".header"));
+class StickyHeader {
+  constructor(t) {
+    (this.header = t),
+      (this.body = document.querySelector("body")),
+      (this.thresholdPosition = 15),
+      (this.triggeredStickyClass = "header--sticky-triggered"),
+      (this.stickyClass = "header--sticky"),
+      (this.ticking = false),
+      (this.bodyPosition = 0),
+      window.addEventListener("DOMContentLoaded", () => this.initSticky()),
+      this.scrollChanged();
+      this.navbar = document.getElementById('navbarSupportedContent');
+      // add window resize listener
+      window.addEventListener("resize", () => this.resizeHandler());
+  }
+  initSticky() {
+    (this.headerStaticHeight = this.header.getBoundingClientRect().height),
+      this.header.classList.toggle(this.stickyClass, true),
+      window.addEventListener("scroll", () => this.scrollHandler());
+      
+  }
+  scrollHandler() {
+    this.ticking ||
+      (window.requestAnimationFrame(() => {
+        this.scrollChanged(), (this.ticking = false);
+      }),
+      (this.ticking = true));
+  }
+  scrollChanged() {
+    (this.bodyPosition = Math.abs(this.body.getBoundingClientRect().top)),
+      this.bodyPosition > this.thresholdPosition
+        ? this.header.classList.toggle(this.triggeredStickyClass, true)
+        : this.header.classList.toggle(this.triggeredStickyClass, false);
+  }
+  resizeHandler() {
+    // if the window size is larger than 992px and the navbar is displayed,
+    // toggle the navbar to hide it
+    if (window.innerWidth > 991) {
+        if (this.navbar?.classList.contains('show')) {
+            simulateClick(document.querySelector('.navbar-toggler'));
+        }
+      }
+  }
+}
+const stickyHeader = new StickyHeader(document.querySelector(".header"));
